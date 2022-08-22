add2796d128fafe69753c80116bd903afd8504ff
Nils K <git@nkn-it.de>
2022-08-22 22:33:49 +0200

    update readme

---
+++ readme.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#
+# Update shield in README.md with date of last build
+#
+# [![Latest build](https://img.shields.io/badge/Last%20build-1900--01--01-blue)](https://github.com/Cyclenerd/google-cloud-gcp-tools-container/actions/workflows/docker-latest.yml)
+#
+
+# GitHub Action runner
+if [ -v GITHUB_RUN_ID ]; then
+	echo "» Set git username and email"
+	git config user.name "github-actions[bot]"
+	git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+fi
+
+export MY_DATE=$(date --utc +'%Y--%m--%d')
+
+perl -i -pe's|\d{4}--\d{2}--\d{2}|$ENV{MY_DATE}|' "README.md"
+
+if ! git diff --exit-code "README.md"; then
+	echo "README.md changed!"
+	git add "README.md" || exit 9
+	git commit -m "Last build" || exit 9
+	git push || exit 9
+else
+	echo "README.md not changed."
+fi
+
+echo "DONE"
